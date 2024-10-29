8c0121706c757396019694ed03e19de162b81ea9
Divyam <139254503+sou7re3ver@users.noreply.github.com>
2024-10-30 00:03:40 +0530

    Create code1.c

---
+++ 29-10-2024/code1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    long long int number, modifiedNumber = 0;
+    int digit, place = 1;
+
+    printf("Enter a number: ");
+    scanf("%lld", &number);
+
+    while (number != 0) {
+        digit = number % 10; 
+        number /= 10; 
+0 
+        if (digit == 0) {
+            digit = 1;
+        } else if (digit == 1) {
+            digit = 0;
+        }
+
+       r
+        modifiedNumber += digit * place;
+        place *= 10; 
+    }
+    printf("Modified number: %lld\n", modifiedNumber);
+
+    return 0;
+}
