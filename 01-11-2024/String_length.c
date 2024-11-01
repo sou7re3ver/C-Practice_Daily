7396dde94e873cfebd69765183d237b2480ef93d
Divyam <139254503+sou7re3ver@users.noreply.github.com>
2024-11-01 22:57:47 +0530

    Create String_length.c

---
+++ 01-11-2024/String_length.c
@@ -0,0 +1,23 @@
+//program to find the lenght of the string
+
+#include<stdio.h>
+#include<string.h>
+#define MAX 100
+#define NIL ('\0')
+
+int findlength(char *s){
+    if(s[0]==NIL){
+        return 0;
+      }
+      return 1+findlength(s+1);
+    }
+
+int main(){
+    char b[MAX];
+    printf("Enter a string\n");
+    scanf("%[^\n]",b);
+    int len;
+    len=findlength(b);
+    printf("%d\n",len);
+   
+   }
